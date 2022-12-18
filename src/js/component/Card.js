76d71fc6416ceb4edaf52dd7e11f274262861fd9
Dalyclnl <dalyarmijoa@yahoo.com>
2022-12-18 12:43:18 +0000

    home finalizado

--- src/js/component/Card.js
+++ src/js/component/Card.js
@@ -1,33 +1,21 @@
 
 import React from "react";
-import ReactDOM from "react-dom";
-import PropType from "prop-types";
 
 
 const Card = (props) => {
 	return (
         <div className="container">
             <div className="card" style="width: 18rem;">
-            <img src={props.imageUrl} className="card-img-top" ></img>
+            <img src= "https://via.placeholder.com/500x325.png" className="card-img-top" ></img>
             </div>
             <div className="card-body">
                 <h5 className="card-title">{props.title}</h5>
                 <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                <a href={props.btnUrl} className="btn btn-primary">{props.btnLabel}</a>
+                <a href='#' className="btn btn-primary">Find out more!</a>
             </div>
         </div>
 	);
 };
-
-Card.propTypes = {
-    btnUrl: PropType.string,
-	btnLabel: PropType.string,
-	title: PropType.string,
-	imgUrl: PropType.string,
-
-};
-
-
 
 
 
