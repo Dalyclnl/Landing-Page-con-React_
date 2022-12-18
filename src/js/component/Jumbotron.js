7fe50336512796c4a33a546157e145d87eb59d3b
Dalyclnl <dalyarmijoa@yahoo.com>
2022-12-18 11:47:25 +0000

    jumbotron listo

--- src/js/component/Jumbotron.js
+++ src/js/component/Jumbotron.js
@@ -1,13 +1,17 @@
 import React from "react";
 
-export const Jumbotron = (props) => {
-	return (
-        <div className="Jumbotron">
-          <h1> A warm Welcome</h1>
-           <p> Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. </p>
-           <a className="btn btn-primary" href="#" role="button">Call to action</a>
-        </div>
-	);
+export const Jumbotron = () => {
+        return (
+        <div className="container jb ">         
+                <div className="Jumbotron">
+                <h1> A warm Welcome</h1>
+                <p> Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. </p>
+                <p className="lead">
+                <a className="btn btn-primary" href="#" role="button">Call to action</a>
+                </p>
+                </div>
+        </div> 
+        );
 };
 
-
+export default Jumbotron;
