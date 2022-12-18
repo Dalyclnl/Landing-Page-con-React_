a4c502bc05b652dd73e68dcb570d01c137a4c9c9
Dalyclnl <dalyarmijoa@yahoo.com>
2022-12-18 21:14:34 +0000

    footer

--- src/js/component/Footer.js
+++ src/js/component/Footer.js
@@ -3,9 +3,19 @@ import React from "react";
 
 export const Footer = (props) => {
 	return (
-      <div>
-            &&&&&&&
-      </div>
+            <div>
+            <footer class="text-center text-white" style="bg-dark text-center text-white;">
+              <div class="container p-4 pb-0">
+                <section class="">
+                  <p class="d-flex justify-content-center align-items-center">
+                  </p>
+                </section>
+              </div
+              <div class="text-center p-3" style="background-color: rgba(0, 0, 0, 0.2);">
+                © 2022 Copyright:DalyAA_Website
+              </div>
+            </footer>
+          </div>
   	);
 };
 
