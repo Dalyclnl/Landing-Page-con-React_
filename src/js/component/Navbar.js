ddb0ac6defdf971a358f756bc94cf4db1047d1e6
Dalyclnl <dalyarmijoa@yahoo.com>
2022-12-18 12:32:01 +0000

    navbar listo

--- src/js/component/Navbar.js
+++ src/js/component/Navbar.js
@@ -1,20 +1,30 @@
 
 import React from "react";
 
-export const Navbar = (props) => {
+const Navbar = () => {
 	return (
-            <div className="Navbar">
-                    <div nav className="navbar navbar-expand-lg navbar-light bg-light">
-                        <a className="navbar-brand" href="#">Navbar</a>
-                        <a href="javascript:void(0)" class="closebtn" onClick="closeNav()">&times;</a>
-                        <a href="#">About</a>
-                        <a href="#">Services</a>
-                        <a href="#">Clients</a>
-                        <a href="#">Contact</a>
-                    </div>
-                    <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-                        <span className="navbar-toggler-icon"></span>
-                    </button>
+        <nav class="navbar navbar-expand-lg navbar-dark bg-dark static-top">
+            <div class="container">
+            <a class="navbar-brand" > Star Bootsrtrap</a>
+                <ul class="navbar-nav ms-auto">
+                    <li class="nav-item">
+                        <a class="nav-link active" aria-current="page" href="#">Home</a>
+                    </li>
+                    <li class="nav-item">    
+                        <a class="nav-link active" aria-current="page" href="#">Abaut</a>
+                     </li>
+                     <li class="nav-item">  
+                        <a class="nav-link active" aria-current="page" href="#">Services</a>
+                    </li>
+                        <a class="nav-link active" aria-current="page" href="#">Services</a>
+                    <li class="nav-item"> 
+                        <a class="nav-link active" aria-current="page" href="#">Contact</a>
+                    </li>  
+                </ul>
             </div>
+        </nav>
 	);
 };
+
+export default Navbar
+
