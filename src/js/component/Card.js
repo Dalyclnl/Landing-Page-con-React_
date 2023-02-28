46d89b9e652c568d61f6079a0c97fb704e1051ea
Dalyclnl <dalyarmijoa@yahoo.com>
2023-02-28 20:27:46 +0000

    card

--- src/js/component/Card.js
+++ src/js/component/Card.js
@@ -1,31 +1,49 @@
-
 import PropType from "prop-types";
 import React from "react";
 
-       
 export const Card = (props) => {
-	return (
-        <div class="card-deck">
-         <div className="card text-top">
-        <img className="card-img-top" src={props.card_imageUrl} alt="Card image cap"></img>
-        <div className="card-body ">
-            <h5 className="card-title">{props.card_title}</h5>
-            <p className="card-text">{props.card_descriptext} </p>
-        </div>
-        <div className="card-footer ">
-            <a href={props.btn_Url} className="btn btn-primary p-2">{props.btn_Label}</a>
-            </div>
-        </div>
-    </div>
-
-	);
+  return (
+        <div className="card-container">
+           <div className="card-deck">
+  <Card
+    card_imageUrl="url_de_la_imagen_1.jpg"
+    card_title="Título de la tarjeta 1"
+    card_descriptext="Descripción de la tarjeta 1"
+    btn_Url="url_del_boton_1"
+    btn_Label="Etiqueta del botón 1"
+  />
+  <Card
+    card_imageUrl="url_de_la_imagen_2.jpg"
+    card_title="Título de la tarjeta 2"
+    card_descriptext="Descripción de la tarjeta 2"
+    btn_Url="url_del_boton_2"
+    btn_Label="Etiqueta del botón 2"
+  />
+  <Card
+    card_imageUrl="url_de_la_imagen_3.jpg"
+    card_title="Título de la tarjeta 3"
+    card_descriptext="Descripción de la tarjeta 3"
+    btn_Url="url_del_boton_3"
+    btn_Label="Etiqueta del botón 3"
+  />
+  <Card
+    card_imageUrl="url_de_la_imagen_4.jpg"
+    card_title="Título de la tarjeta 4"
+    card_descriptext="Descripción de la tarjeta 4"
+    btn_Url="url_del_boton_4"
+    btn_Label="Etiqueta del botón 4"
+  />
+</div>
+        </div> 
+  );
 };
+
+
+
 Card.propTypes = {
-    card_imageUrl: PropType.string,
-	card_title: PropType.string,
-	card_descriptext: PropType.string,
-	btn_Url: PropType.string,
-	btn_Label: PropType.string,
+  card_imageUrl: PropType.string,
+  card_title: PropType.string,
+  card_descriptext: PropType.string,
+  btn_Url: PropType.string,
+  btn_Label: PropType.string,
 };
-
-
--- src/js/component/Jumbotron.js
+++ src/js/component/Jumbotron.js
@@ -1,21 +1,22 @@
 import React from "react";
 
-
 export const Jumbotron = () => {
-	return (
-        <div className="container jumbo ">
-                <div className="jumbotron">
-                <h1 className="display-4">
-                A Warm Welcome!
-                </h1>
-                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Diam in arcu cursus euismod quis viverra nibh cras. Potenti nullam ac tortor vitae purus faucibus ornare suspendisse.</p>
-                <p className="lead">
-                <a className="btn btn-primary" href="#" role="button">Call to action!</a>
-                </p>
-                </div>
-                </div>
-	);
+  return (
+    <div className="container jumbo ">
+      <div className="jumbotron">
+        <h1 className="display-4">A Warm Welcome!</h1>
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+          eiusmod tempor incididunt ut labore et dolore magna aliqua. Diam in
+          arcu cursus euismod quis viverra nibh cras. Potenti nullam ac tortor
+          vitae purus faucibus ornare suspendisse.
+        </p>
+        <p className="lead">
+          <a className="btn btn-primary" href="#" role="button">
+            Call to action!
+          </a>
+        </p>
+      </div>
+    </div>
+  );
 };
-
-
-
