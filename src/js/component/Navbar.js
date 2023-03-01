64c2dff021f7b9acc19ea191fd9d58b035476fa5
Dalyclnl <dalyarmijoa@yahoo.com>
2023-03-01 00:19:18 +0000

    finalizado

--- src/js/component/Navbar.js
+++ src/js/component/Navbar.js
@@ -5,7 +5,7 @@ export const Navbar = () => {
 	return (
         <nav className ="navbar navbar-expand-lg navbar-dark bg-dark static-top">
             <div className ="container nav">
-                <a className="navbar-brand" > Star Bootsrtrap</a>
+                <a className="navbar-brand" > Star Bootstrap</a>
                 <ul className="navbar-nav ms-auto">
                     <li className="nav-item">
                         <a className="nav-link active" aria-current="page" href="#">Home</a>
